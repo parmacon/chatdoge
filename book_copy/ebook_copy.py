243f46f5eb625769c19b2e6081ebcc41699c66de
parmacon <42090680+parmacon@users.noreply.github.com>
2024-03-06 02:17:13 +0900

    12

--- book_copy/ebook_copy.py
+++ book_copy/ebook_copy.py
@@ -2,25 +2,19 @@ import pyautogui as auto
 import time
 import pyperclip
 
-
-
-
 #북큐브
 total_page =3
 book_name='Node200'
 capture_pos=(1392,145)
 ebook_screen_center_pos=(515,606)
 ebook_screen_right_pos=(997,579)
-sleep_time =1
-
+sleep_time =0.5
 
 for i in range(int(total_page)):
     time.sleep(sleep_time)
 
     file_name = book_name + '_'+ str(i+1).zfill(4) + '.png'
     pyperclip.copy(file_name)
-
-    time.sleep(sleep_time)
 
     auto.click(capture_pos)
     time.sleep(sleep_time)
@@ -34,7 +28,7 @@ for i in range(int(total_page)):
     auto.press('enter')
     time.sleep(sleep_time)
     
-    
-    print(file_name +'_done')
+    auto.click(ebook_screen_right_pos)
+    time.sleep(sleep_time)
 
     
