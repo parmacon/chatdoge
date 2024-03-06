9d684714225c36aec15d560786d9f8895809bb51
parmacon <42090680+parmacon@users.noreply.github.com>
2024-03-06 23:26:35 +0900

    1`

--- book_copy/ebook_copy.py
+++ book_copy/ebook_copy.py
@@ -3,24 +3,22 @@ import time
 import pyperclip
 
 #북큐브
-total_page =3
-book_name='Node200'
-capture_pos=(1392,145)
+total_page =392
+book_name='부의 추월 차선'
+capture_pos=(245,117)
 ebook_screen_center_pos=(515,606)
-ebook_screen_right_pos=(997,579)
+ebook_screen_right_pos=(907,502)
 sleep_time =0.5
 
 for i in range(int(total_page)):
-    time.sleep(sleep_time)
-
     file_name = book_name + '_'+ str(i+1).zfill(4) + '.png'
     pyperclip.copy(file_name)
 
     auto.click(capture_pos)
     time.sleep(sleep_time)
 
-    auto.click(ebook_screen_center_pos)
-    time.sleep(sleep_time)
+    #auto.click(ebook_screen_center_pos)
+    #time.sleep(sleep_time)
 
     auto.hotkey('ctrl','v')
     time.sleep(sleep_time)
